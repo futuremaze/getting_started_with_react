796da90c3b972ced8d963ead06076c03db19c1d5
onodak <future.maze@gmail.com>
2021-03-30 05:11:05 +0000

    Header, Footer のコンポーネント化
    今から始めるReact入門 〜 React の基本 #1

--- react-tutorial/src/js/components/Layout.js
+++ react-tutorial/src/js/components/Layout.js
@@ -1,24 +1,14 @@
 import React from "react";
+import Header from "./Header"
+import Footer from "./Footer"
 
 // 外部から参照できるようにexport構文を使用する
 export default class Layout extends React.Component {
-    constructor() {
-        super();
-        this.name = "Tsutomu";
-    }
     render() {
-        let name = "Tsutomu";
         return (
             <div>
-                {/* 変数を埋め込む場合は {変数名} とする */}
-                <h1>It's {name}!</h1>
-                {/* 計算式も書ける */}
-                <h1>It's {1 + 2}!</h1>
-                {/* 関数も書ける */}
-                <h1>It's {this.get_result(3)}!</h1>
-                {/* 匿名関数でもよい */}
-                <h1>It's {((num) => { return 1 + num; })(3)}!</h1>
-                <h1>It's {this.name}!</h1>
+                <Header />
+                <Footer />
             </div>
         );
     }
