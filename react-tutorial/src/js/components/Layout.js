59cc601b616bec937380de602e161964afcc5586
onodak <future.maze@gmail.com>
2021-03-30 05:27:35 +0000

    state について
     今から始めるReact入門 〜 React の基本 #1

--- react-tutorial/src/js/components/Layout.js
+++ react-tutorial/src/js/components/Layout.js
@@ -4,9 +4,18 @@ import Footer from "./Footer"
 
 // 外部から参照できるようにexport構文を使用する
 export default class Layout extends React.Component {
+    constructor() {
+        super();
+        this.state = { name: "Tsutomu" };
+    }
     render() {
+        setTimeout(
+            () => { this.setState({ name: "Hello" }) },
+            1000
+        );
         return (
             <div>
+                {this.state.name}
                 <Header />
                 <Footer />
             </div>
