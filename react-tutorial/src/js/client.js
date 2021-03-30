62b24217d0ebabb644937693afa81f8af28bbef1
onodak <future.maze@gmail.com>
2021-03-30 04:57:59 +0000

    変数の取り扱いについて
    今から始めるReact入門 〜 React の基本 #1

--- react-tutorial/src/js/client.js
+++ react-tutorial/src/js/client.js
@@ -2,10 +2,28 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 class Layout extends React.Component {
+    constructor() {
+        super();
+        this.name = "Tsutomu";
+    }
     render() {
+        let name = "Tsutomu";
         return (
-            <h1>Welcome!</h1>
+            <div>
+                {/* 変数を埋め込む場合は {変数名} とする */}
+                <h1>It's {name}!</h1>
+                {/* 計算式も書ける */}
+                <h1>It's {1 + 2}!</h1>
+                {/* 関数も書ける */}
+                <h1>It's {this.get_result(3)}!</h1>
+                {/* 匿名関数でもよい */}
+                <h1>It's {((num) => { return 1 + num; })(3)}!</h1>
+                <h1>It's {this.name}!</h1>
+            </div>
         );
+    }
+    get_result(num) {
+        return 1 + num;
     }
 }
 
